2c182b68dea8a0542e71753e04745acdb8d2d4c8
Matthew C <mchen146@ucsc.edu>
2023-06-03 14:17:16 -0700

    decorated ui for title and level select

--- scenes/main_scenes/level_select/level_select.gd
+++ scenes/main_scenes/level_select/level_select.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var level_container = $VBoxContainer/GridContainer
+	var level_container = %LevelContainer
 	
 	# add levels
 	
