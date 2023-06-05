8de9793c9458af005912daaf6bd2da02934e9a36
MarRod12 <91565970+MarRod12@users.noreply.github.com>
2023-06-04 19:42:10 -0700

    added text font and filled out controls screen

--- scenes/main_scenes/Settings/settings.gd
+++ scenes/main_scenes/Settings/settings.gd
@@ -3,7 +3,9 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready(): 
-	pass
+	%backButton.pressed.connect(func():
+		get_tree().change_scene_to_file("res://scenes/main_scenes/title/title.tscn")
+	)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
