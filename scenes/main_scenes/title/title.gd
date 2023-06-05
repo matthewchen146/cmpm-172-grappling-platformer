0b7fdefd8e1a2f8f05d396083e34a01642aae5ca
MarRod12 <91565970+MarRod12@users.noreply.github.com>
2023-06-04 17:48:39 -0700

    creating settings scene

--- scenes/main_scenes/title/title.gd
+++ scenes/main_scenes/title/title.gd
@@ -6,6 +6,9 @@ func _ready():
 	%PlayButton.pressed.connect(func():
 		get_tree().change_scene_to_file("res://scenes/main_scenes/level_select/level_select.tscn")
 	)
+	%SettingsButton.pressed.connect(func(): 
+		get_tree().change_scene_to_file("res://scenes/main_scenes/settings.tscn")
+	)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
