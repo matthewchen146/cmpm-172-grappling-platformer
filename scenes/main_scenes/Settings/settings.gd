db3cb1dade1a17548685cd911d23fd07c443d202
MarRod12 <91565970+MarRod12@users.noreply.github.com>
2023-06-04 20:10:25 -0700

    working on back button

--- scenes/main_scenes/Settings/settings.gd
+++ scenes/main_scenes/Settings/settings.gd
@@ -3,9 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready(): 
-	%backButton.pressed.connect(func():
-		get_tree().change_scene_to_file("res://scenes/main_scenes/title/title.tscn")
-	)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
