10571988b66a4910995d5e7db00857ac82418069
MarRod12 <91565970+MarRod12@users.noreply.github.com>
2023-06-05 18:54:20 -0700

    fixed buttons

--- scenes/main_scenes/settings/settings.gd
+++ scenes/main_scenes/settings/settings.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	%returnButton.pressed.connect(func():
+	%backButton.pressed.connect(func():
 		get_tree().change_scene_to_file("res://scenes/main_scenes/title/title.tscn")
 	)
 
