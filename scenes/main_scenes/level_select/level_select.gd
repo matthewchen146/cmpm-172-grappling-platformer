292d3b135338a322e7d1a82e671a4196f2747906
Peyton <projectile212@gmail.com>
2023-06-07 18:38:38 -0700

    new level who dis (6)

--- scenes/main_scenes/level_select/level_select.gd
+++ scenes/main_scenes/level_select/level_select.gd
@@ -12,7 +12,8 @@ func _ready():
 		{"path": "res://scenes/main_scenes/Levels/Level2.tscn", "name": "Level 2"},
 		{"path": "res://scenes/main_scenes/Levels/Level3.tscn", "name": "Level 3"},
 		{"path": "res://scenes/main_scenes/Levels/Level4.tscn", "name": "Level 4"},
-		{"path": "res://scenes/main_scenes/Levels/Level5.tscn", "name": "Level 5"}
+		{"path": "res://scenes/main_scenes/Levels/Level5.tscn", "name": "Level 5"},
+		{"path": "res://scenes/main_scenes/Levels/Level6.tscn", "name": "Level 6"}
 	]
 	
 	var level_number = 1
