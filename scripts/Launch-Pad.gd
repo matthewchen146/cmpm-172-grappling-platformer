1043087c393419138f6c7196e041970714ba8ea2
Ben Quang <100402290+benqUC@users.noreply.github.com>
2023-06-10 16:07:22 -0700

    Added launchpad sfx

--- scripts/Launch-Pad.gd
+++ scripts/Launch-Pad.gd
@@ -1,5 +1,6 @@
 extends Node
 
+@onready var LaunchPadSFX = $"Launch-PadSFX"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,4 +19,5 @@ func Body_Entered_Jump_Region(body):
 		body.linear_velocity.y = 0
 		body.apply_impulse(Vector2(0, -1000))
 		body.linear_damp = 0.05
+		LaunchPadSFX.play()
 	pass # Replace with function body.
