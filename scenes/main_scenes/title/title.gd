fa76a1119eb5bf4430a85a97e62b867a62382514
Newone88 <81717053+Newone88@users.noreply.github.com>
2023-06-11 20:30:21 -0700

    Added PlayerSpalshScreenToTitle

--- scenes/main_scenes/title/title.gd
+++ scenes/main_scenes/title/title.gd
@@ -3,6 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	%PlayButton.pressed.connect(func():
 		#get_tree().change_scene_to_file("res://scenes/main_scenes/level_select/level_select.tscn")
 		var scene_trs = load("res://scenes/main_scenes/level_select/level_select.tscn")
@@ -30,3 +31,4 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
